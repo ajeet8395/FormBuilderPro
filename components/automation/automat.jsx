317dc2b8c87c26e4ba4715c649be9e53025aae45
ajeet8395 <ajeetsi8395@gmail.com>
2024-09-08 11:52:34 +0530

    add use client

--- components/automation/automat.jsx
+++ components/automation/automat.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link";
 import Accordion from "../accordion";
 import AutomatForm from "./automatForm";
--- components/automation/sidebar.jsx
+++ components/automation/sidebar.jsx
@@ -1,3 +1,4 @@
+"use client";
 import { useState } from "react";
 import Accordion from "../accordion";
 import Link from "next/link";
